e9b9ed6d4890f104f59227998d4a1fa02d9d3faa
Joys <joys.tower@gmail.com>
2016-03-04 12:42:03 -0600

    selectors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var lMap;
 /*global L*/
 
 var mexLayer;
+var mexGeoJSON;
 var mexData;
 var mexVariables;
 
@@ -15,13 +16,19 @@ var scaleColor;
 
 $(document).ready(function(){
 
+  $('#inpCols').val(arrColors.toString());
+  $('#inpCols').on('change',function(){
+    arrColors = $(this).val().split(',');
+    colorLayer();
+  });
+
 // dark_all  light_all
   var lyLight = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    //attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
   });
 
   var lyDark = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',{
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    attribution: '&copy;<br> <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
   });
 
   var lyToner = new L.StamenTileLayer("toner");
@@ -59,70 +66,8 @@ $(document).ready(function(){
 
 });
 
-
-function addData(){
-  d3.json( 'data/mgm2015v6_2_4326.topojson', function ( e, json ) {
-    var canvas = L.canvas();
-    d3.tsv( 'data/ITER_NALMUN_10_utf.tsv', function ( e, data ) {
-      mexData = data;
-      mexVariables = _.pairs(mexData[0]).map(function(pair){return pair[0];});
-      mexLayer = topojson.feature( json, json.objects['mgm2015v6_2_4326'] );
-
-      var mData = (_.pluck(mexData, dat2Show));
-      dmax = _.max(mData.map(function(o){return parseInt(o) || 0;}));
-      dmin = _.min(mData.map(function(o){return parseInt(o) || 0;}));
-      scaleColor = chroma.bezier(arrColors);
-      scaleColor = chroma.scale(scaleColor).domain([dmin,dmax], 1 ).correctLightness(true);
-
-      lMap.addLayer(
-        new L.GeoJSON(mexLayer, {
-          renderer: canvas,
-          smoothFactor: 0.3,
-
-          onEachFeature: function ( feature, layer ) {
-            layer.on( {
-              mouseover: function() {
-                var p = feature.properties;
-                var d = _.where(mexData,{'ENTIDAD':p.ent,'MUN':p.mun})[0];
-                if(d){
-                  $('.overInfo').html(d.NOM_ENT+" - "+d.NOM_MUN+' ('+dat2Show+': '+(parseInt(d[dat2Show]).toLocaleString())+')');
-                }
-              },
-              mouseout: function() {
-                $('.overInfo').html('Localidades');
-              }
-            });
-          },
-
-          style: function( feature ) {
-            var p = feature.properties;
-            var d = _.where(mexData,{'ENTIDAD':p.ent,'MUN':p.mun})[0];
-            if(!d){
-              //console.log('ENTIDAD:'+p.ent+' MUN:'+p.mun);
-              return {
-                fillOpacity: 0.65,
-                fillColor: '#000000',
-                stroke: 0
-              };
-            }
-            var color = scaleColor( (parseInt(d[dat2Show]) || 0) ).hex();
-            return {
-              fillOpacity: 0.65,
-              fillColor: color,
-              stroke: 0
-            };
-          }
-        })
-      );
-
-      addLegend();
-    });
-  });
-}
-
-
-
 function addLegend(){
+  $('#Info').html('');
   var range = $('<div />').css('width', '92%').addClass('overInfo-range').appendTo('#Info');
   var lab = $('<div />').html('Municipio...').css('width', '92%').addClass('overInfo').appendTo('#Info');
   var div = $('<div />').css('width', '92%').addClass('gradient').appendTo('#Info');
@@ -137,4 +82,101 @@ function addLegend(){
   }
   var dL = $('<div />').addClass('infL').html(parseInt(dmin).toLocaleString()).appendTo(range);
   var dR = $('<div />').addClass('infR').html(parseInt(dmax).toLocaleString()).appendTo(range);
+}
+
+function colorLayer(){
+  var mData = (_.pluck(mexData, dat2Show));
+  dmax = _.max(mData.map(function(o){return parseInt(o) || 0;}));
+  dmin = _.min(mData.map(function(o){return parseInt(o) || 0;}));
+  scaleColor = chroma.bezier(arrColors);
+  scaleColor = chroma.scale(scaleColor).domain([dmin,dmax], 1 ).correctLightness(true);
+
+  mexLayer.clearLayers();
+  mexLayer.addData(mexGeoJSON);
+
+  addLegend();
+}
+
+function addDataCensus(){
+  d3.tsv( 'data/ITER_NALMUN_10_utf.tsv', function ( e, data ) {
+    mexData = data;
+    mexVariables = _.pairs(mexData[0]).map(function(pair){return pair[0];});
+
+    _.each(mexVariables,function(o){
+      if(['ENTIDAD','NOM_ENT','MUN','NOM_MUN','LOC'].indexOf(o) !== -1){
+        return;
+      }
+      $('#selVars').append('<option value="'+o+'">'+o+'</option>');
+    });
+
+    $('#selVars').on('change',function(){
+      dat2Show = $(this).val();
+      colorLayer();
+    });
+
+    var mData = (_.pluck(mexData, dat2Show));
+    dmax = _.max(mData.map(function(o){return parseInt(o) || 0;}));
+    dmin = _.min(mData.map(function(o){return parseInt(o) || 0;}));
+    scaleColor = chroma.bezier(arrColors);
+    scaleColor = chroma.scale(scaleColor).domain([dmin,dmax], 1 ).correctLightness(true);
+
+    colorLayer();
+  });
+}
+
+function addData(){
+  d3.json( 'data/mgm2015v6_2_4326.topojson', function ( e, json ) {
+    mexGeoJSON = topojson.feature( json, json.objects.mgm2015v6_2_4326 );
+    var canvas = L.canvas();
+
+
+    mexLayer = new L.GeoJSON(mexGeoJSON, {
+        renderer: canvas,
+        smoothFactor: 0.3,
+
+        onEachFeature: function ( feature, layer ) {
+          layer.on( {
+            mouseover: function() {
+              if(feature && feature.properties && mexData){
+                var p = feature.properties;
+                var d = _.where(mexData,{'ENTIDAD':p.ent,'MUN':p.mun})[0];
+                if(d){
+                  $('.overInfo').html(d.NOM_ENT+" - "+d.NOM_MUN+' ('+dat2Show+': '+(parseInt(d[dat2Show]).toLocaleString())+')');
+                }
+              }
+            },
+            mouseout: function() {
+              $('.overInfo').html('Localidades');
+            }
+          });
+        },
+
+        style: function( feature ) {
+          if(!feature){
+            return {
+              fillOpacity: 0.20,
+              fillColor: '#000000',
+              stroke: 0
+            };
+          }
+          var p = feature.properties;
+          var d = _.where(mexData,{'ENTIDAD':p.ent,'MUN':p.mun})[0];
+          if(!d){
+            return {
+              fillOpacity: 0.20,
+              fillColor: '#000000',
+              stroke: 0
+            };
+          }
+          var color = scaleColor( (parseInt(d[dat2Show]) || 0) ).hex();
+          return {
+            fillOpacity: 0.65,
+            fillColor: color,
+            stroke: 0
+          };
+        }
+      });
+    lMap.addLayer( mexLayer );
+    addDataCensus();
+  });
 }
